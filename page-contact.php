2bbaddb9cd4877bed6e0aead95d8460bd5e666aa
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-18 17:54:52 +0900

    ファイル追加

---
+++ page-contact.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+    <main id="page-contact">
+      <div class="directory">
+        <div class="inner">
+          <a href="/">ホーム</a>
+          <span>&gt;</span>
+          <span>お問い合わせ</span>
+        </div>
+      </div>
+
+      <section id="contact-top">
+          <div class="outer">
+            <h2 class="lower-h2">お問い合わせ</h2>
+          </div>
+          <p class="main-text">
+            お問合せ内容によっては、お時間を頂戴する場合がございます。<br>
+            また、ご返答を差し上げられない場合もございますので、あらかじめご了承ください。<br>
+            必要事項にご入力の上、「送信」ボタンを押してください。
+          </p>
+          <?php the_content(); ?>
+      </section>
+    </main>
+<?php get_footer(); ?>
---
+++ page-employment.php
@@ -0,0 +1,211 @@
+<?php get_header(); ?>
+
+    <main id="page-employment" class="businesses">
+      <div class="directory">
+        <div class="inner">
+          <a href="/">ホーム</a>
+          <span>&gt;</span>
+          <span>就労継続支援B型グレースハウス／ポケット</span>
+        </div>
+      </div>
+
+      <section id="employment-top" >
+        <div class="outer">
+          <h2 class="lower-h2">就労継続支援B型</h2>
+        </div>
+
+        <div class="lower-keyvisual">
+            <img class="pc" src="<?= get_theme_file_uri(); ?>/img/employment-keyvisual.png" alt="">
+            <img class="sp" src="<?= get_theme_file_uri(); ?>/img/employment-keyvisual-sp.png" alt="">
+            <div class="text">
+                <p>
+                    人とくらべない、人のことを気にしない<br>
+                    自分を<span>大切</span>にし、いたわり<span>癒</span>す<br>
+                    かけがえのない<span>存在</span>の支え合いの<span>空間</span>
+                </p>
+            </div>
+        </div>
+
+        <h3><span>グレースハウスと<br>ポケットについて </span><img src="<?= get_theme_file_uri(); ?>/img/h2-icon05.png" alt=""></h3>
+
+
+        <div class="businesses-inner">
+          <div class="employment-about">
+            <h4>大切にしていること</h4>
+            <p>私はどうしたいの？その気持ちを大切にしています。無理をせずに自身をラクな気持ちにするための正直な答えが最善であることをいつも共有し合っています。「できる」、「できない」で自分の可能性を決めつけず、自分がやりたいことが回復につながっていることを信じて取り組める時間と空間を提供しています。人間関係が苦手という方もコミュニケーションはあいさつだけで大丈夫です。失敗バンザイ、誰もあなたを責めません。自分のよいところ、人のよいところを発見しながら自分に心地よいリズムで過ごしてください。</p>
+          </div>
+          <div class="employment-about">
+            <h4>こんなことをしています</h4>
+            <dl class="employment-dl">
+              <div>
+                <dt><span></span>着物のリメイク</dt>
+                <dd>寄付していただいた着物をリメイクしています。</dd>
+              </div>
+              <div>
+                <dt><span></span>グループワーク</dt>
+                <dd>自分のよいところ、人のよいところを発見する時間。<br>（月１回開催 / 自由参加）</dd>
+              </div>
+              <div>
+                <dt><span></span>歌の会</dt>
+                <dd>お互いの響きを感じ合いながら歌います。<br>（月１回開催 / 自由参加）</dd>
+              </div>
+              <div>
+                <dt><span></span>課外研修</dt>
+                <dd>一人だと不安だけれどみんなでなら楽しめる体験。<br>（年１回開催 / 自由参加）</dd>
+              </div>
+              <div>
+                <dt><span></span>その他</dt>
+                <dd>ゲーム機の洗浄、ゴミステーションの掃除、<br>出店販売など</dd>
+              </div>
+            </dl>
+          </div>
+
+        </div>
+        <?php get_template_part("parts/contact");?>
+      </section>
+
+      <div class="businesses-images">
+        <div class="inner">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment01.png" alt="">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment02.png" alt="">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment03.png" alt="">
+
+        </div>
+      </div>
+
+      <section id="businesses-flow">
+        <div class="businesses-inner">
+          <h4>グレースハウスのとある1日の流れ</h4>
+          <ul>
+            <li>
+              <time>10:00〜</time>
+              <span></span>
+              <div class="text">
+                <h5>開所</h5>
+                <p>朝は健康チェックの検温から始まります。コロナ対策だけでなく体調を考慮してその日のサポートを行います。</p>
+              </div>
+            </li>
+            <li>
+              <time>10:20〜</time>
+              <span></span>
+              <div class="text">
+                <h5>朝ミーティング</h5>
+                <p>ミーティングが終わればまずは掃除。その後、各々がその日に取り組みたい仕事を決めて始めます。</p>
+              </div>
+            </li>
+            <li>
+              <time>12:00〜</time>
+              <span></span>
+              <div class="text">
+                <h5>昼休憩</h5>
+                <p>再度それぞれが取り組みたい仕事を開始します。利用者同士でカフェの接客練習をすることも。</p>
+              </div>
+            </li>
+            <li>
+              <time>15:00〜</time>
+              <span></span>
+              <div class="text">
+                <h5>閉所・送迎（火曜日と金曜日は14時閉所）</h5>
+                <p>閉所後は送迎車にて、ご自宅までお送りします。</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+
+      <div class="businesses-images">
+        <div class="inner">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment04.png" alt="">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment05.png" alt="">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment06.png" alt="">
+        </div>
+      </div>
+
+
+      <section id="businesses-flow">
+        <div class="businesses-inner">
+          <h4>ポケットのとある1日の流れ</h4>
+          <ul>
+            <li>
+              <time>10:00〜</time>
+              <span></span>
+              <div class="text">
+                <h5>開所</h5>
+                <p>朝は健康チェックの検温から始まります。コロナ対策だけでなく体調を考慮してその日のサポートを行います。</p>
+              </div>
+            </li>
+            <li>
+              <time>10:20〜</time>
+              <span></span>
+              <div class="text">
+                <h5>朝ミーティング</h5>
+                <p>ミーティングが終わればまずは掃除。その後、各々がその日に取り組みたい仕事を決めて始めます。</p>
+              </div>
+            </li>
+            <li>
+              <time>12:00〜</time>
+              <span></span>
+              <div class="text">
+                <h5>昼休憩</h5>
+                <p>再度それぞれが取り組みたい仕事を開始します。利用者同士でカフェの接客練習をすることも。</p>
+              </div>
+            </li>
+            <li>
+              <time>12:50〜</time>
+              <span></span>
+              <div class="text">
+                <h5>昼ミーティング</h5>
+                <p>ミーティングが終われば、再度それぞれが取り組みたい仕事を開始します。</p>
+              </div>
+            </li>
+            <li>
+              <time>15:00〜</time>
+              <span></span>
+              <div class="text">
+                <h5>閉所・送迎（火曜日と金曜日は14時閉所）</h5>
+                <p>閉所後は送迎車にて、ご自宅までお送りします。</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>      
+
+      <div class="businesses-images">
+        <div class="inner">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment07.png" alt="">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment08.png" alt="">
+          <img src="<?= get_theme_file_uri(); ?>/img/employment09.png" alt="">
+
+        </div>
+      </div>
+
+      <section id="shops">
+        <h4>アトリエりらでは、各事業所で制作した作品を販売しています。<br>お気軽にお立ち寄りください。</h4>
+        <div>
+          <div class="shop">
+            <img src="<?= get_theme_file_uri(); ?>/img/gracehouse.png" alt="">
+            <div class="text">
+              <h5>グレースハウス</h5>
+              <address>〒590-0811　堺市堺区南陵町1-2-6<br>TEL：072-280-0830　FAX：072-241-7772</address>
+              <a class="access" href="https://www.google.com/maps/place/%E3%80%92590-0811+%E5%A4%A7%E9%98%AA%E5%BA%9C%E5%A0%BA%E5%B8%82%E5%A0%BA%E5%8C%BA%E5%8D%97%E9%99%B5%E7%94%BA%EF%BC%91%E4%B8%81%EF%BC%92%E2%88%92%EF%BC%96+%E3%82%B5%E3%83%AF%E3%83%AA/@34.5542581,135.4666439,17z/data=!3m1!4b1!4m6!3m5!1s0x6000db67fac93337:0xbd35ce8027ee33f7!8m2!3d34.5542581!4d135.4692135!16s%2Fg%2F11d_4fj8fw?entry=ttu">アクセス（GoogleMap）<span>→</span></a>
+              <p>[堺市指定事業所番号]<br>2716001587号</p>
+            </div>
+          </div>
+          <div class="shop">
+            <img src="<?= get_theme_file_uri(); ?>/img/pocket.png" alt="">
+            <div class="text">
+              <h5>グレースハウス</h5>
+              <address>〒590-0811　堺市堺区南陵町1-2-6<br>TEL：072-280-0830　FAX：072-241-7772</address>
+              <a class="access" href="https://www.google.com/maps/place/%E3%80%92590-0811+%E5%A4%A7%E9%98%AA%E5%BA%9C%E5%A0%BA%E5%B8%82%E5%A0%BA%E5%8C%BA%E5%8D%97%E9%99%B5%E7%94%BA%EF%BC%91%E4%B8%81%EF%BC%92%E2%88%92%EF%BC%96+%E3%82%B5%E3%83%AF%E3%83%AA/@34.5542581,135.4666439,17z/data=!3m1!4b1!4m6!3m5!1s0x6000db67fac93337:0xbd35ce8027ee33f7!8m2!3d34.5542581!4d135.4692135!16s%2Fg%2F11d_4fj8fw?entry=ttu">アクセス（GoogleMap）<span>→</span></a>
+              <p>[堺市指定事業所番号]<br>2716001587号</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <?php get_template_part("parts/business"); ?>
+      <?php get_template_part("parts/middle"); ?>
+
+    </main>
+<?php get_footer(); ?>
