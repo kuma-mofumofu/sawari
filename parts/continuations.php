c8711e9b4cccda86fdf8a5f65c242ce79c05de5e
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-18 17:56:47 +0900

    ファイル追加

---
+++ parts/continuations.php
@@ -0,0 +1,4 @@
+      <div class="continuations">
+          <a href="/about/" class="continuation"><span class="color">サワリ</span>について</a>
+          <a href="/company/" class="continuation">法人概要</a>
+      </div>
