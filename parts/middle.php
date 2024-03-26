89f1505a2165a5b021bd95566b006d0f3e26d7d7
kuma-mofumofu <toucyann.910@gmail.com>
2024-03-26 16:48:39 +0900

    スペース調整

--- parts/middle.php
+++ parts/middle.php
@@ -4,7 +4,7 @@
           <img src="<?= get_theme_file_uri(); ?>/img/love-icon.png" alt="">
           <span>支援のお願い</span>
         </a>
-        <a href="/journal/" class="tag">
+        <a href="/ecclesia/" class="tag">
           <img src="<?= get_theme_file_uri(); ?>/img/newspaper-icon.png" alt="">
           <span>機関紙<br>エクレーシア</span>
         </a>
